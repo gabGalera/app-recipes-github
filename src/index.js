5dd9e528077f98f38e0f3e0067200b488b454b89
Gabriel Galera <gabgalera@hotmail.com>
2022-12-22 10:14:40 -0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux';
@@ -11,9 +11,9 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider store={ store }>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   );
 
